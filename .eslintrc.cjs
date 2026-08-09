f1c2422f6750379ea4375de0754e15dcc97a3783
AmirHossein <amirhossein.shk@gmail.com>
2026-08-09 13:44:34 +0330

    feat: init eslint and prettier configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['@nuxt/eslint-config', 'plugin:prettier/recommended'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': 'error',
+  },
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,18 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // Your custom configs here
-)
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  prettier
+);
